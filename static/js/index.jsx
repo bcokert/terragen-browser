f0acee5c37cce542fdfddd71646d122fc18faba4
Brandon Okert <brandon.okert@hootsuite.com>
2016-06-06 10:27:24 -0700

    Added plotting library and basic test plot. Added eslint and configured dev server

--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,12 +1,66 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+'use strict';
+var React =  require('react');
+var ReactDom = require('react-dom');
+var Plotly = require('plotly.js');
+var _ = require('underscore');
 
 require('./index.less');
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this._layout = {
+            title: 'Test Noise'
+        };
+
+        this._config = {
+            showLink: false,
+            displayModeBar: true
+        };
+
+        this.onClickRandomize = this.onClickRandomize.bind(this);
+    }
+
+    componentDidMount() {
+        Plotly.plot(this._node, [this.props.plot], this._layout, this._config);
+    }
+
+    componentDidUpdate() {
+        Plotly.redraw(this._node);
+    }
+
+    componentWillUnmount() {
+        var cruft = document.getElementById('js-plotly-tester');
+        cruft.parentNode.removeChild(cruft);
+    }
+
+    onClickRandomize() {
+        fakeData.y = _.range(0,100).map(x => Math.random());
+        this.forceUpdate();
+    }
+
     render () {
-        return <p>Hello!</p>;
+        return <div className='App' onClick={this.onClickRandomize} ref={node => this._node = node}></div>;
     }
 }
 
-ReactDom.render(<App/>, document.getElementById('app'));
+App.displayName = 'App';
+
+App.propTypes = {
+    plot: React.PropTypes.shape({
+        x: React.PropTypes.arrayOf(React.PropTypes.number),
+        y: React.PropTypes.arrayOf(React.PropTypes.number),
+        mode: React.PropTypes.string,
+        name: React.PropTypes.string
+    })
+};
+
+let fakeData = {
+    x: _.range(0,100),
+    y: _.range(0,100).map(x => Math.random()),
+    mode: 'lines',
+    name: 'noise 1'
+};
+
+ReactDom.render(<App plot={fakeData}/>, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,28 @@
-var webpack = require('webpack');
+'use strict';
+require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'build');
 var APP_DIR = path.resolve(__dirname, 'static/js');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.jsx?/, include: APP_DIR, loader: 'babel' },
-      { test: /\.css$/, loader: "style!css" },
-      { test: /\.less/, loader: "style!css!less" },
-      { test: /\.json$/, loader: 'json'}
-    ]
-  }
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        publicPath: '/static/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.jsx?/, include: APP_DIR, loader: 'babel'},
+            {test: /\.css$/, loader: 'style!css'},
+            {test: /\.less/, loader: 'style!css!less'},
+            {test: /\.json$/, loader: 'json'}
+        ],
+        noParse: [
+            /plotly\.js/
+        ]
+    }
 };
 
 module.exports = config;
